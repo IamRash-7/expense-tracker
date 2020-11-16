18726e3e67c960ddc70011fa61c891c88a3b3eb3
IamRash-7 <rasswanthshankar@rocketmail.com>
2020-11-16 23:36:39 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Header} from './components/Header.js';
+import {Balance} from './components/Balance.js';
+import {Inex} from './components/Inex.js';
+import {Trlist} from './components/Trlist.js';
+import {Transaction} from './components/Transaction.js';
+
+import {GlobalProvider} from './context/global';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <GlobalProvider>
+      <Header/>
+      <div className='container'>
+        <Balance />
+        <Inex />
+        <Trlist/>
+        <Transaction/>
+      </div>
+    </GlobalProvider>
   );
 }
 
