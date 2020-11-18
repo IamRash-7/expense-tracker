03fd96858abdddc401c5332c77c3686ea3e5f3a8
IamRash-7 <rasswanthshankar@rocketmail.com>
2020-11-18 10:23:25 +0530

    frontend done

--- src/components/Trlist.js
+++ src/components/Trlist.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React,{useContext} from 'react';
+import {GlobalContext} from '../context/global';
+
+import {Transele} from './Transele'
 
 export const Trlist = () => {
+    const {transactions}=useContext(GlobalContext);
+
+
     return (
         <div>
             <h3>History</h3>
             <ul className="list">
-            <li className="minus">
-            Cash <span>-$400</span><button className="delete-btn">x</button>
-            </li> 
+            {transactions.map(transaction=>(<Transele key={transaction.id} transaction={transaction}/>))}
             </ul>
         </div>
     )
